82f42c0ccd2492f1fe84a8bf0e7d82bc646a9d40
annammattis <54290779+annammattis@users.noreply.github.com>
2020-02-03 17:47:55 -0500

    pseudo code

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+// Your server.js file should require the basic npm packages we've used in class: express and path.
+
 var express = require('express');
 var path = require('path');
 
 var app = express();
-
 var PORT = process.env.PORT || 8080;
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
  
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
