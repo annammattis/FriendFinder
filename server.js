4ac12e7e92f9fb0ec473ff924a4a1a9baddbd38c
annammattis <54290779+annammattis@users.noreply.github.com>
2020-02-03 18:24:41 -0500

    server.js done

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Your server.js file should require the basic npm packages we've used in class: express and path.
-
 var express = require('express');
 var path = require('path');
 
@@ -9,6 +8,13 @@ var PORT = process.env.PORT || 8080;
 // app.get('/', function (req, res) {
 //   res.send('Hello World')
 // })
+
+// For serving of static CSS
+app.use(express.static(__dirname + "/app/css"));
+
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
  
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
