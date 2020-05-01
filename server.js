a3626f9767a8e3dea2abfe8ac66dca908b56e729
annammattis <54290779+annammattis@users.noreply.github.com>
2020-05-01 14:12:51 -0400

    changed app.use in server

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// For serving of static CSS
-app.use(express.static(__dirname + "/app/css"));
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // API and HTML routes
 require("./app/routing/apiRoutes.js")(app);
