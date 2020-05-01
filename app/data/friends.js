9460b6b3554bac8b49355a0483e12ea18367050b
annammattis <54290779+annammattis@users.noreply.github.com>
2020-05-01 15:02:13 -0400

    added urls

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,113 +9,93 @@
 var friends = [
     {
         "name": "Livvy",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/siqEBD5uGJjCdWJY8",
         "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
         "name": "Marie",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/mfmuPNq1AsKBz7ty9",
         "scores": ["5", "2", "4", "5", "5", "2", "2", "4", "4", "2"]
     },
     {
         "name": "Hannah",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/UfntyBTTyJGgbKTLA",
         "scores": ["6", "1", "3", "4", "6", "1", "3", "5", "5", "1"]
     },
     {
         "name": "Brenda",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/W18ir3WTvmD4XReW6",
         "scores": ["6", "2", "4", "3", "5", "2", "2", "4", "4", "2"]
     },
     {
         "name": "Jamie",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/zAkWh5FiNAB2hy746",
         "scores": ["7", "1", "5", "4", "6", "1", "1", "5", "3", "1"]
     },
     {
         "name": "Chelsea",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/6BmbTYYZtdG9wvjU9",
         "scores": ["7", "2", "4", "3", "5", "2", "2", "6", "4", "2"]
     },
     {
         "name": "Alex",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/xWVuDH97GfwwcWeW8",
         "scores": ["8", "7", "1", "6", "2", "8", "7", "3", "4", "1"]
     },
     {
         "name": "Liz",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/BamYhGqNxWrHA6947",
         "scores": ["8", "9", "5", "1", "9", "6", "8", "2", "6", "7"]
     },
     {
         "name": "Kendall",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/CXm5T9q3PNw92zye6",
         "scores": ["9", "2", "3", "7", "2", "6", "3", "4", "3", "8"]
     },
     {
-        "name": "Dana",
-        "photo": "URL",
-        "scores": ["9", "1", "4", "8", "1", "9", "7", "6", "1", "8"]
-    },
-    {
         "name": "Kate",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/AQWbzxEoi53Dpayt5",
         "scores": ["1", "9", "3", "6", "2", "6", "3", "4", "7", "8"]
     },
     {
         "name": "Sam",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/p85ZCMUgkRUFKvaB9",
         "scores": ["4", "6", "8", "3", "1", "3", "5", "7", "9", "3"]
     },
     {
         "name": "Andrew",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/oeDCkjBD9PXfytsi6",
         "scores": ["4", "5", "7", "9", "5", "4", "3", "6", "8", "1"]
     },
     {
         "name": "Jake",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/HpAJoYN5N8eAAq6g8",
         "scores": ["1", "3", "5", "7", "9", "6", "4", "3", "6", "2"]
     },
     {
         "name": "Dan",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/cRyfUij6oT62ci7a6",
         "scores": ["2", "3", "6", "8", "9", "5", "3", "5", "2", "5"]
     },
     {
-        "name": "Gavin",
-        "photo": "URL",
-        "scores": ["2", "4", "6", "7", "8", "9", "3", "1", "3", "5"]
-    },
-    {
         "name": "Adam",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/BL77EX92HK7ejTMv8",
         "scores": ["9", "7", "5", "4", "3", "2", "5", "2", "6", "2"]
     },
     {
         "name": "Chris",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/6q72BB81kiucMMtk8",
         "scores": ["8", "5", "3", "2", "5", "2", "5", "1", "6", "1"]
     },
     {
         "name": "Kitt",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/43RABUAHvRw6ad2U7",
         "scores": ["9", "5", "4", "7", "3", "7", "2", "1", "3", "8"]
     },
     {
         "name": "Connor",
-        "photo": "URL",
+        "photo": "https://photos.app.goo.gl/eFzkfh1FTKsaGg566",
         "scores": ["1", "4", "6", "8", "9", "5", "3", "2", "2", "5"]
-    },
-    {
-        "name": "Scott",
-        "photo": "URL",
-        "scores": ["3", "5", "7", "9", "4", "1", "4", "8", "3", "5"]
-    },
-    {
-        "name": "Luke",
-        "photo": "URL",
-        "scores": ["4", "5", "2", "6", "1", "6", "3", "7", "8", "4"]
     }
 ];
 
