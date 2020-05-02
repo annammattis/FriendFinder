eeaa012394c973fd899892329edb4a808217df5b
annammattis <54290779+annammattis@users.noreply.github.com>
2020-05-02 11:22:57 -0400

    added home path

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "./app/public/home.html"));
+  });
+
 // express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
 // The code below is pretty standard.
 app.use(express.urlencoded({ extended: true }));
